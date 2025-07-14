753cbc4d2d7388053dfe85409aa18a34ce07e6db
jamesbeedy <jamesbeedy@gmail.com>
2025-07-14 09:10:50 +0300

    deduplicate package.json

---
+++ copy-package-json-to-labextension.js
@@ -0,0 +1,14 @@
+// Script to copy package.json to the labextension folder
+const fs = require('fs');
+const path = require('path');
+
+const src = path.join(__dirname, 'package.json');
+const destDir = path.join(__dirname, 'jupyterlab_firefox_launcher', 'labextension');
+const dest = path.join(destDir, 'package.json');
+
+if (!fs.existsSync(destDir)) {
+  fs.mkdirSync(destDir, { recursive: true });
+}
+
+fs.copyFileSync(src, dest);
+console.log(`Copied package.json to ${dest}`);
